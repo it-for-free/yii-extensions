a0f13a59720ca091d39e5b8445e254edde610096
Web-Programmer-B-P <barsukov2805@gmail.com>
2018-12-21 18:38:13 +0300

    Добавлена функция по обрезку картинок

---
+++ src/Yii2/Module/Image/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+
+if (!function_exists('imgHandl')) {
+
+    /**
+     * Метод обёртка над img с возможностью обрезки фото на лету
+     * @param $src путь к изображению
+     * @param $format формат обрезки изображения
+     * @param null $options
+     * @return img
+     */
+    function imgHandl($src, $format, $options = null)
+    {
+        return Html::img('/image?path=' . $src . '&format=' . $format, $options);
+    {
+
+}
